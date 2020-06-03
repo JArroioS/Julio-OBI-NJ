59fa501c11a21e3a190a1b85f6825aaba709d0df
JG248 <49651793+JG248@users.noreply.github.com>
2020-06-03 13:37:49 -0300

    Add files via upload

---
+++ Basquete de Robôs 2018 - Fase 1/Basquete de Robôs 2018 - Fase 1 C++/Basquete de Robôs 2018 - Fase 1 C++/Basquete de Robôs 2018 - Fase 1 C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int D;
+
+	cout << "Qual a distancia do robo?";
+	cin >> D;
+
+	if (D <= 800)
+	{
+		cout << "1";
+	}
+	else if ((800 < D) && (D <= 1400))
+	{
+		cout << "2";
+	}
+	else if ((1400 < D) && (D <= 2000))
+	{
+		cout << "3";
+	}
+}
